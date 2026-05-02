1f866d36fa63f212f5a30948ae8a6a7d30bb5292
ExpandablePanda <info@andrewmarkschaffer.com>
2026-05-02 07:12:02 -0400

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["127.0.0.1"],
+  output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/photo-1494790108377-be9c29b29330",
-        search: "?auto=format&fit=crop&w=900&q=80",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/photo-1500648767791-00dcc994a43e",
-        search: "?auto=format&fit=crop&w=200&q=80",
-      },
-    ],
+    unoptimized: true,
   },
+  allowedDevOrigins: ["127.0.0.1"],
 };
 
 export default nextConfig;
